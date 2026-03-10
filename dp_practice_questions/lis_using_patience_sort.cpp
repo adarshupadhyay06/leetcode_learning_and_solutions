bc479d7828666c0cb36a2dbf21c7cf7997d34b26
adarshupadhyay06 <163729421+adarshupadhyay06@users.noreply.github.com>
2026-03-10 23:17:16 +0530

    DP: Longest Increasing Subsequence using Patience Sorting

---
+++ dp_practice_questions/lis_using_patience_sort.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> sorted;
+        
+        for(int i = 0; i<n; i++) {
+           
+            auto it = lower_bound(begin(sorted), end(sorted), nums[i]);
+            
+            if(it == end(sorted))
+                sorted.push_back(nums[i]); 
+            else
+                *it = nums[i]; 
+        }
+        
+        return (int)sorted.size();
+    }
+};
